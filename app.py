7b537b082e29f42178f0ca59ea2c4e9d349e97b7
wendybovill <wendybovill@gmail.com>
2023-05-31 20:58:28 +0000

    important files:requirements, app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import (
+    Flask, flash, render_template, redirect, request, session, url_for)
+from flask_pymongo import flask_pymongo
+from bson.objectid import objectid
+from werkzeug.security import generate_passowrd_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
