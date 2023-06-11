234f68e549a38b155b6f931b73e29fb18a381df4
wendybovill <wendybovill@gmail.com>
2023-06-11 18:16:38 +0000

    changed verification method for emails. fixed styling for dates and forms

--- app.py
+++ app.py
@@ -198,7 +198,19 @@ def verify_email(username):
         users = mongo.db.users.find().sort("username", 1)
         user = mongo.db.users.find_one({"username": username})
         flash("Your email has been verified " + username)
-        return redirect(url_for("log_in"))
+
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username")})
+            
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get(
+                        "password")):
+                session["user"] = request.form.get("username")
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
 
     users = mongo.db.users.find().sort("username", 1)
     user = mongo.db.users.find_one({"username": username})
