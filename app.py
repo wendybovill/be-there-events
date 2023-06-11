a65f84aacea91d04d65f339e1b93523007bbe30c
wendybovill <wendybovill@gmail.com>
2023-06-11 10:40:44 +0000

    refactoring code for login

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ def log_in():
             {"username": request.form.get("email")})
         form_email = request.form.get("email")
 
-        verify_query = mongo.db.users.find(
+        verify_query = mongo.db.users.find_one(
             {"username": request.form.get("username")}).sort("verified", 1)
 
         if existing_username:
